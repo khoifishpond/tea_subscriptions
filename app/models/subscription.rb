c7d9214738e9d51154f539eec69a83971522b004
Khoi Nguyen <khoinguyen311@gmail.com>
2022-01-11 19:53:47 -0800

    Create customer and subscription models

---
+++ app/models/subscription.rb
@@ -0,0 +1,16 @@
+class Subscription < ApplicationRecord
+  belongs_to :tea
+  belongs_to :customer
+
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :status, presence: true
+  validates :frequency, presence: true
+  validates :tea_id, presence: true
+  validates :customer_id, presence: true
+
+  enum status: {
+    cancelled: 0,
+    active: 1
+  }
+end
